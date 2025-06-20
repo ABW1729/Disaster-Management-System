2dae7f064581937e55c328ece19baad4b26ad07d
Aniket Wani <101266471+ABW1729@users.noreply.github.com>
2025-06-20 18:26:09 +0530

    Update socket.js

--- disaster-dashboard/src/socket.js
+++ disaster-dashboard/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://51.20.134.106:5000', {
+const socket = io('https://disaster-management-system-production-22c3.up.railway.app:5000', {
   withCredentials: true,
 });
 
